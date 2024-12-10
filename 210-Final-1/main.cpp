98a870a426a6d31d1774d3e5377cebb036d0a5fe
Errant27 <temporal.cerauno@gmail.com>
2024-12-09 18:54:23 -0800

    commit 2

--- 210-Final-1/main.cpp
+++ 210-Final-1/main.cpp
@@ -1,8 +1,12 @@
 // 210 | Final | Neil Orton
 // IDE used: Xcode
 #include <iostream>
+#include <iomanip>
 #include <deque>
+#include <random>
 using namespace std;
+const int W = 3;
+int rand_num();
 
 struct CoffeeBooth {
 private:
@@ -17,22 +21,50 @@ private:
     CoffeeBooth *next;
 
 public:
+CoffeeBooth() { head = nullptr; }
+    
 void push_back(string nam, string ord) {
     CoffeeBooth *newCustomer = new CoffeeBooth(nam, ord);
     if(!head)
         head = newCustomer;
-    else
+    else {
+        head->next = newCustomer;
+        cout << setw(W) << newCustomer->name << " added to the line" << endl;
+    }
+}
 
+void print_booth() {
+        
 }
 
 };
 
 int main() {
     
+    CoffeeBooth booth;
+    string name;
+    string order;
+    
     string names[5] = {"Daniel", "Erica", "Lily", "Moique", "Nathan"};
     string drinks[3] = {"Coffee", "Latte", "Espresso"};
     
+    cout << "Coffee Booth Initial cue:\n";
+    for (int i = 0; i < 3; ++i) {
+        int num = rand_num();
+        name = names[num];
+        order = drinks[num];
+        
+        booth.push_back(name, order);
+    }
     
     
     return 0;
+}
+
+int rand_num() {    // Function to generate random number
+    random_device randNum;
+    uniform_int_distribution<int>range(0, 4);
+    int num = range(randNum);
+    
+    return num;
 }
