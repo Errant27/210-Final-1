1780df0a8b3632fb5ad367654cafb39795ad04fa
Errant27 <temporal.cerauno@gmail.com>
2024-12-09 17:40:04 -0800

    Initial Commit

---
+++ 210-Final-1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  210-Final-1
+//
+//  Created by Neil Orton on 12/9/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
